006989e0262876ae1429cea6e175f44b7750ea23
Ryo-Takahashi-0422 <rtakahashi0422@yahoo.co.jp>
2023-07-15 12:08:57 +0900

    外部ｼﾝﾎﾞﾙｴﾗｰ解消。別のｲﾝﾀｰﾌｪｰｽ実装ｸﾗｽのｸﾗｽ名をﾒｿｯﾄﾞに::していたことが原因...

--- DirectX12_Play/PeraGraphicsPipelineSetting.h
+++ DirectX12_Play/PeraGraphicsPipelineSetting.h
@@ -1,20 +1,20 @@
-//#pragma once
-//
-//class PeraGraphicsPipelineSetting : public IGraphicsPipelineSetting
-//{
-//
-//private:
-//	D3D12_INPUT_ELEMENT_DESC inputLayouts[2];
-//
-//public:
-//	PeraGraphicsPipelineSetting(VertexInputLayout* _vertexInputLayout);
-//	~PeraGraphicsPipelineSetting();
-//	HRESULT CreateGPStateWrapper(ComPtr<ID3D12Device> _dev,
-//		SetRootSignature* setRootSignature, ComPtr<ID3D10Blob> _vsBlob, ComPtr<ID3D10Blob> _psBlob);
-//
-//	void SetInputlayout(int i, D3D12_INPUT_ELEMENT_DESC inputLayout);
-//	D3D12_GRAPHICS_PIPELINE_STATE_DESC SetGPL(
-//		SetRootSignature* setRootSignature, ComPtr<ID3D10Blob> _vsBlob, ComPtr<ID3D10Blob> _psBlob);
-//
-//	ComPtr<ID3D12PipelineState> GetPipelineState();
-//};
+#pragma once
+
+class PeraGraphicsPipelineSetting : public IGraphicsPipelineSetting
+{
+
+private:
+	D3D12_INPUT_ELEMENT_DESC inputLayouts[2];
+
+public:
+	PeraGraphicsPipelineSetting(VertexInputLayout* _vertexInputLayout);
+	~PeraGraphicsPipelineSetting();
+	HRESULT CreateGPStateWrapper(ComPtr<ID3D12Device> _dev,
+		SetRootSignature* setRootSignature, ComPtr<ID3D10Blob> _vsBlob, ComPtr<ID3D10Blob> _psBlob);
+
+	void SetInputlayout(int i, D3D12_INPUT_ELEMENT_DESC inputLayout);
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC SetGPL(
+		SetRootSignature* setRootSignature, ComPtr<ID3D10Blob> _vsBlob, ComPtr<ID3D10Blob> _psBlob);
+
+	ComPtr<ID3D12PipelineState> GetPipelineState();
+};
