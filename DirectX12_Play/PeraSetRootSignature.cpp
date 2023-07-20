6c19e0e7547c6751bbedf6bc5711725d5786b65b
Ryo-Takahashi-0422 <rtakahashi0422@yahoo.co.jp>
2023-07-20 22:22:08 +0900

    マルチパス理解資料追加

--- DirectX12_Play/PeraSetRootSignature.cpp
+++ DirectX12_Play/PeraSetRootSignature.cpp
@@ -2,8 +2,6 @@
 #include <PeraSetRootSignature.h>
 
 HRESULT PeraSetRootSignature::SetRootsignatureParam(ComPtr<ID3D12Device> _dev) {
-	//●リソース初期化
-	// 初期化処理1：ルートシグネチャ設定
 
 	//サンプラー作成
 	sampler = CD3DX12_STATIC_SAMPLER_DESC(0);
@@ -11,17 +9,9 @@ HRESULT PeraSetRootSignature::SetRootsignatureParam(ComPtr<ID3D12Device> _dev) {
 	sampler.AddressV = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
 
 	//サンプラーのスロット設定
-	//descTableRange[0].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0);
-	//descTableRange[1].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 1);
+	descTableRange[0].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0);
+	descTableRange[1].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 1);
 	
-	descTableRange[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
-	descTableRange[0].BaseShaderRegister = 0;
-	descTableRange[0].NumDescriptors = 1;
-
-	descTableRange[1].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
-	descTableRange[1].BaseShaderRegister = 1;
-	descTableRange[1].NumDescriptors = 1;
-
 	rootParam[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
 	rootParam[0].DescriptorTable.NumDescriptorRanges = 1;
 	rootParam[0].DescriptorTable.pDescriptorRanges = &descTableRange[0];
@@ -37,10 +27,6 @@ HRESULT PeraSetRootSignature::SetRootsignatureParam(ComPtr<ID3D12Device> _dev) {
 	rootSignatureDesc.NumStaticSamplers = 1;
 	rootSignatureDesc.pStaticSamplers = &sampler;
 	rootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
-
-	//rootSignatureDesc.NumParameters = 0;
-	//rootSignatureDesc.NumStaticSamplers = 0;
-	//rootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 
 	result = D3D12SerializeRootSignature //シリアル化
 	(
